31cde5b4293f8bfa37d4d1d88c4671295f908311
tcmalware <genarodvo@gmail.com>
2022-06-26 17:29:29 -0500

    Agregar archivos de proyecto.

---
+++ CRUD14/CRUD14/DataContext/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRUD14
+{
+    public class AppDbContext : DbContext
+    {
+        //Path donde se va guardar la base de datos
+        string DbPath = string.Empty;
+
+
+        //Tablas de base de datos
+        public DbSet<Student> People { get; set; }
+
+
+
+        //Estándar del desarrollo con EFC
+        public AppDbContext(string dbPath)
+        {
+            this.DbPath = dbPath;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Filename={DbPath}");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(Student).Assembly);
+        }
+    }
+}
